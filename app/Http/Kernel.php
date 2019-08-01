0fa5b5eae2f3ac6f7790e1bd6d270b6b6ddb072d
shook <liushoukun66@gmail.com>
2019-08-01 10:06:48 +0800

    Cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\SetLocale::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -61,6 +62,7 @@ class Kernel extends HttpKernel
         'signed'        => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle'      => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'      => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors'          => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
